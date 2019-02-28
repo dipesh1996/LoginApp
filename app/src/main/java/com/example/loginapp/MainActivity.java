4c1f9e1bf52534aa4644a07a5dcef996aec2c2cd
Dipesh Rafaliya <dipeshrafalia@gmail.com>
2019-02-28 00:32:42 -0800

    This File has the Login verification while the user sign in using the file operations.

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.loginapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,18 +52,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void validate()
-    {
-        String userName = username.getText().toString().trim();
-        String userPassword = password.getText().toString().trim();
-          if (userName.equals("admin") && userPassword.equals("admin")) {
-                Toast.makeText(MainActivity.this, "Log In Successful", Toast.LENGTH_SHORT).show();
-            } else {
-              Toast.makeText(MainActivity.this, "Username or Password Incorrect ", Toast.LENGTH_SHORT).show();
-                count = count + 1;
-                if(count>2)
-                    System.exit(0);
+    public void readData(String user, String pass) {
+        try {
+            int flag =0;
+            File root = Environment.getExternalStorageDirectory();
+            File file = new File(root, "loginapp.txt");
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(",");
+                String TempUser = tokens[4];
+                String TempPass = tokens[5];
+                if (user.equals(TempUser) && pass.equals(TempPass)) {
+                    flag = 0;
+                    Toast.makeText(MainActivity.this, "Successfull Login", Toast.LENGTH_SHORT).show();
+                }
+            }
+                bufferedReader.close();
+        }
+        catch(Exception ex)
+            {
+                ex.printStackTrace();
             }
     }
+    private void validate() {
+        String uname = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        try {
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                if (uname.isEmpty() || pass.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter username or password", Toast.LENGTH_SHORT).show();
+                } else
+                    readData(uname, pass);
+            } else {
+                requestStoragePermissions();
+                if (uname.isEmpty() || pass.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please Enter username or password", Toast.LENGTH_SHORT).show();
+                } else
+                    readData(uname,pass);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+        private void requestStoragePermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        new AlertDialog.Builder(this)
+        .setTitle("permission needed").setMessage("Storage permission needed")
+        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+        @Override
+            public void onClick(DialogInterface dialog, int which) {
+             }
+        }).setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+        @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        }).create().show();
+        } else
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+    }
 }
+
+
+
+
